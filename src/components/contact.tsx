165dcf6ac5cb3012ba55fb23cd59a25d5543e612
Lajamnong <ploitii6@gmail.com>
2025-05-07 11:11:27 +0700

    ปรับ responsive

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,15 +1,14 @@
-import { useTranslation } from 'react-i18next'; // นำเข้า useTranslation
-import "./skills.css"; // เปลี่ยนเป็นตัวพิมพ์เล็ก
+import { useTranslation } from 'react-i18next';
+import "./skills.css";
 // src/components/Contact.jsx
 
 const Contact = () => {
-  const { t } = useTranslation();  // เรียกใช้ t() จาก useTranslation
-
+  const { t } = useTranslation(); 
   return (
     <section id="contact" className="contact">
       <p>{t('Contact')}</p> 
       <p>Email: 📧 ploitii6@gmail.com</p>
-      <p>GitHub: 🐙 <a href="https://github.com/Ploitiiq">https://github.com/Ploitiiq</a></p>
+      <p>GitHub: 🐙 <a href="https://github.com/Ploitiiiq">https://github.com/Ploitiiiq</a></p>
     </section>
   );
 }
